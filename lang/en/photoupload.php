aa5b1501201f64abcb01f2df721060bc080fa799
rundownrob1 <47049015+rundownrob1@users.noreply.github.com>
2025-03-06 17:48:39 -0800

    Create photoupload.php

---
+++ lang/en/photoupload.php
@@ -0,0 +1,17 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+$string['modulename'] = 'Photo upload';
+$string['modulenameplural'] = 'Photo uploads';
+$string['modulename_help'] = 'A custom activity that allows users to capture or upload photos and send them to a third-party service.';
+$string['photoupload:addinstance'] = 'Add a new photo upload activity';
+$string['photoupload:view'] = 'View photo upload activity';
+$string['pluginname'] = 'Photo upload';
+$string['fallbackuploadlabel'] = 'Or upload an image file:';
+$string['uploadsuccess'] = 'Your photo has been successfully uploaded.';
+$string['uploaderror'] = 'There was an error uploading your photo.';
+$string['capturebutton'] = 'Capture From Camera';
+$string['submitbutton'] = 'Submit Photo';
+$string['introtext'] = 'Use the camera below or upload a photo file.';
+$string['privacy:metadata'] = 'The Photo Upload plugin does not store personal data beyond user-submitted images.';
+
